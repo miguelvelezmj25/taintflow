bacf211bd6cddab223beead62bca710ac43ca940
Miguel Velez <miguelvelez@mijecu25.com>
2017-08-04 08:19:17 -0400

    Added test

--- soot-infoflow/test/soot/jimple/infoflow/test/options/OptionsTest.java
+++ soot-infoflow/test/soot/jimple/infoflow/test/options/OptionsTest.java
@@ -208,4 +208,19 @@ public class OptionsTest {
         checkInfoflow(infoflow, 1);
     }
 
+    @Test
+    public void sleep7Test() throws IOException {
+        Infoflow infoflow = new Infoflow();
+        infoflow.setConfig(OptionsTest.infoflowConfiguration);
+
+//        EasyTaintWrapper easyWrapper = new EasyTaintWrapper(new File("/Users/mvelezce/Documents/Programming/Java/Projects/taint-analysis/soot-infoflow/EasyTaintWrapperSource.txt"));
+//        infoflow.setTaintWrapper(easyWrapper);
+
+        List<String> entryPoints = new ArrayList<>();
+        entryPoints.add("<soot.jimple.infoflow.test.options.code.Basic7: void main(java.lang.String[])>");
+
+        infoflow.computeInfoflow(OptionsTest.appPath, OptionsTest.libPath, entryPoints, OptionsTest.sources, OptionsTest.sinks);
+        checkInfoflow(infoflow, 3);
+    }
+
 }
---
+++ soot-infoflow/test/soot/jimple/infoflow/test/options/code/Basic7.java
@@ -0,0 +1,39 @@
+package soot.jimple.infoflow.test.options.code;
+
+import soot.jimple.infoflow.test.options.Options;
+
+public class Basic7 {
+
+    private static Options options = new Options();
+
+    public static void main(String[] args) throws InterruptedException {
+        boolean A = Basic7.options.getOption();
+        int x = 0;
+
+        if(Basic7.options.getDecision(A)) {
+            x += foo(true);
+        }
+
+        if(Basic7.options.getDecision(x == 1)) {
+            x++;
+        }
+
+        x = foo(true);
+
+        if(Basic7.options.getDecision(x==0)) {
+            x = -1;
+        }
+
+    }
+
+    public static int foo(boolean x) {
+        int i = 0;
+
+        if(Basic7.options.getDecision(x)) {
+            i++;
+        }
+
+        return i;
+    }
+
+}
