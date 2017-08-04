76de044548cc8fb886843eb52f6207645e45a694
Miguel Velez <miguelvelez@mijecu25.com>
2017-08-04 14:47:09 -0400

    Running exeamples

--- src/main/java/edu/cmu/cs/mvelezce/analysis/TaintInfoflow.java
+++ src/main/java/edu/cmu/cs/mvelezce/analysis/TaintInfoflow.java
@@ -3,7 +3,13 @@ package edu.cmu.cs.mvelezce.analysis;
 import soot.jimple.infoflow.Infoflow;
 import soot.jimple.infoflow.solver.cfg.IInfoflowCFG;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TaintInfoflow extends Infoflow {
+
+    private static List<String> sources = new ArrayList<>();
+    private static List<String> sinks = new ArrayList<>();
 
     public IInfoflowCFG getiCfg() {
         return this.iCfg;
