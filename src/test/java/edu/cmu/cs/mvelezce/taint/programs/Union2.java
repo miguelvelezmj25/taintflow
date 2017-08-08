20503ba4d20ef135b8a9181b82794c7c297a58d4
Miguel Velez <miguelvelez@mijecu25.com>
2017-08-08 09:42:01 -0400

    Fixed C getOption

--- src/test/java/edu/cmu/cs/mvelezce/taint/programs/Union2.java
+++ src/test/java/edu/cmu/cs/mvelezce/taint/programs/Union2.java
@@ -34,24 +34,24 @@ public class Union2 {
 
         Thread.sleep(1);
 
-        m1(false, null, null);
+        m1();
 
         if(Sink.getDecision(a)) {
             Thread.sleep(2);
-            m1(false, null, null);
+            m1();
         }
 
-        m1(false, null, null);
+        m1();
 
         if(Sink.getDecision(b)) {
             Thread.sleep(3);
-            m1(false, null, null);
+            m1();
         }
 
-        m1(false, null, null);
+        m1();
     }
 
-    public static void m1(boolean x, String y, Object z) throws InterruptedException {
+    public static void m1() throws InterruptedException {
         Thread.sleep(4);
         Thread.sleep(5);
 
