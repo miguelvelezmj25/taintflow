31e8137c5a7079cb8415aff82d9548445085b379
Miguel Velez <miguelvelez@mijecu25.com>
2017-08-25 14:30:52 -0400

    Added test with reference to object

---
+++ src/test/java/edu/cmu/cs/mvelezce/taint/programs/Interaction9.java
@@ -0,0 +1,46 @@
+package edu.cmu.cs.mvelezce.taint.programs;
+
+import edu.cmu.cs.mvelezce.analysis.option.Sink;
+import edu.cmu.cs.mvelezce.analysis.option.Source;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+    Object
+ */
+public class Interaction9 {
+
+    private boolean b;
+    private boolean c;
+
+    public Interaction9(boolean b, boolean c) {
+        this.b = b;
+        this.c = c;
+    }
+
+    public static void main(String[] args) {
+        Sink.init();
+
+        boolean A = Source.getOptionA(true);
+        boolean B = Source.getOptionB(true);
+
+        boolean a = false;
+        boolean b = false;
+
+        if(A) {
+            a = true;
+        }
+
+        if(B) {
+            b = true;
+        }
+
+        Interaction9 i = new Interaction9(a, b);
+
+        if(i == null) {
+            System.out.println();
+        }
+    }
+
+}
